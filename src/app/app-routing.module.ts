9aa1b1c9e6b14b7ee90a34172de95da357c1eeab
ajaneh <ajh363@nau.edu>
2019-09-06 11:05:19 -0500

    initial build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainComponent} from './main/main.component';
+import {AboutComponent} from './about/about.component';
+import {PhotosComponent} from './photos/photos.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: MainComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'photos',
+    component: PhotosComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { PhotosComponent } from './photos/photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutComponent,
+    PhotosComponent
   ],
   imports: [
     BrowserModule,
