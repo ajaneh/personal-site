1d9ad12a1b479f3cf4617ccadf1e6e993e813647
ajaneh <ajh363@nau.edu>
2019-09-11 10:06:08 -0500

    basic site is setup

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public worksPlaces = {
+    eFuneral: false,
+    research: false,
+    teaching: false,
+    undergrad: false,
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public togglePanel(experience: string, displayDetails: boolean) {
+    this.worksPlaces[experience] = displayDetails;
+  }
+
+  public scroll(el: any) {
+    el.scrollIntoView();
+    console.log(el)
+    console.log(this.worksPlaces[el])
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
 import { PhotosComponent } from './photos/photos.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { PhotosComponent } from './photos/photos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
